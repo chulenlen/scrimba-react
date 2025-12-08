06ecd18f0864d37da6c511874aad700ea54c5def
chulenlen <renzellbolanteofficial@gmail.com>
2025-12-08 22:42:07 +0800

    first react activity

---
+++ first-react/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <img src="public/react-logo.png" alt="React logo" />
+        <span>ReactFacts</span>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
