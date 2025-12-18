fdb0d99d76117110d324d21ebf03f0c1dfeea19a
chulenlen <renzellbolanteofficial@gmail.com>
2025-12-18 21:35:51 +0800

    fix: convert to spread the props

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -8,17 +8,7 @@ function App() {
       <Header />
       <main className="container">
         {data.map(data => {
-          return (
-            <Entry
-              key={data.id}
-              img={data.img}
-              country={data.country}
-              title={data.title}
-              googleMapsLink={data.googleMapsLink}
-              dates={data.dates}
-              text={data.text}
-            />
-          );
+          return <Entry key={data.id} {...data} />;
         })}
       </main>
     </>
