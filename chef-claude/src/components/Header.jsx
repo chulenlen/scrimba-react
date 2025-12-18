c7594e676bd8ea3909ebaf29936c2b9b7ef38b9c
chulenlen <renzellbolanteofficial@gmail.com>
2025-12-19 00:49:52 +0800

    Add chef-claude project

---
+++ chef-claude/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeIcon from '../assets/images/chef-claude-icon.png';
+
+export default function Header() {
+  return (
+    <header>
+      <img src={chefClaudeIcon} alt="Chef Claude Icon" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
